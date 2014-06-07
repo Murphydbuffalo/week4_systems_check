811842772f151063692769e9294e2c5d33457fe8
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-07 14:13:49 -0400

    Defined the BoundingArea class to, if given some number of BoundingBoxes as arguments, check if a given point is within any of those boxes.

--- bounding_areas/lib/bounding_area.rb
+++ bounding_areas/lib/bounding_area.rb
@@ -1,2 +1,13 @@
 class BoundingArea
+	attr_reader :boxes
+	
+	def initialize(boxes)
+    @boxes = boxes
+	end
+
+	def contains_point?(x, y)
+    boxes.any? do |box|
+      box.contains_point?(x, y)
+    end
+	end
 end
